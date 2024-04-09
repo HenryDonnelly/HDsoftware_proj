01434d1c9f539583b5e467f49bb371894b3899ee
henry <you@example.com>
2024-04-09 19:07:43 +0100

    dashboard css tailwind

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,128 +5,206 @@
         </h2>
     </x-slot>
 
-    <div class="container bg-white">
-            <div class="flex justify-center">
-                <img src="storage/rewards/profile-1335-svgrepo-com.png" alt="" width="600">
-            </div>
+    {{-- <div class="container height: 50%;">
+      <div class="flex justify-center">
+          <img src="storage/rewards/profile-1335-svgrepo-com.png" style="width: 15%; display: block; margin: 0 auto; py-5">
+      </div>
+      <div class="justify-center">
+          <div class="font-bold mb-2">
+              <h1 class="text-center" style="font-size: 2vw;">23</h1>
+          </div>
+      </div>
+      <div class="justify-center">
+        <div class="font-bold mb-2">
+            <h1 class="text-center" style="font-size: 2vw;">Total E-coins</h1>
+        </div>
+      </div>
+    </div> --}}
+
+    <div class="container h-50vh flex flex-col justify-center items-center">
+        <img src="storage/rewards/pngwing.com.png" width="40%" style="margin-top: 20px;">
+        <div class="text-center p-2">
+          <h1 class="" style="font-size: 2vw;"> Your ecocoins </h1>
+        </div>
+        <div class="text-center" style="padding-bottom: 10px;">
+            <h1 class="" style="font-size: 2vw;"> 2 </h1>
+        </div>
+      </div>
+
+
+
+
+    <div class="container bg-red-100">
         <div class="flex justify-center">
-                <div class="font-bold text-xl mb-2">E-coins</div>
-           </div>
-    </div>
-
-
-{{-- <div class="container flex">
-    <div class="w-screen flex-grow px-3 py-3">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-            <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
-            <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p class="text-gray-700 text-base">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
-                    perferendis eaque, exercitationem praesentium nihil.
+            <div class="p-1 ">
+                <div class="bg-red-100" style="padding-top: 20px;">
+                    <img src="storage/rewards/PngItem_2128990.png" style="width: 50%; margin: 0 auto; py-5 bg-white " />
+                </div>
+                <h4 class="mb-2 text-center" style="font-size: 2vw;">
+                    Lyft launching cross-platform service this week!
+                </h4>
+                <p class="block mb-8 font-sans text-center" style="font-size: 1.2vw;">
+                    Like so many organizations these days, Autodesk is a company in
+                    transition. It was until recently a traditional boxed software company
+                    selling licenses. Yet its own business model disruption is only part of
+                    the story
                 </p>
-            </div>
-            <div class="px-6 py-4">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+                <div class="flex justify-center mt-2">
+                  <a href="#">
+                    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                      Button
+                    </button>
+                </a>
+                </div>
             </div>
         </div>
-    </div>
-</div> --}}
 
-<div class="container">
 
-<div class="relative flex bg-clip-border rounded-xl bg-white text-gray-700 shadow-md w-full max-w-[48rem] flex-row">
-    <div
-      class="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
-      <img
-        src="https://tailwindcss.com/img/card-top.jpg"
-        alt="card-image" class="object-cover w-full h-full" />
-    </div>
-    <div class="p-6">
-      <h6 class="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700 uppercase">
-        startups
-      </h6>
-      <h4 class="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-        Lyft launching cross-platform service this week
-      </h4>
-      <p class="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
-        Like so many organizations these days, Autodesk is a company in
-        transition. It was until recently a traditional boxed software company
-        selling licenses. Yet its own business model disruption is only part of
-        the story
-      </p>
-      <a href="#" class="inline-block"><button
-          class="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/10 active:bg-gray-900/20"
-          type="button">
-          Learn More<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-            stroke-width="2" class="w-4 h-4">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path>
-          </svg></button></a>
-    </div>
-  </div>
 
-  <div class="relative flex bg-clip-border rounded-xl bg-white text-gray-700 shadow-md w-full max-w-[48rem] flex-row">
-    <div
-      class="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
-      <img
-        src="https://tailwindcss.com/img/card-top.jpg"
-        alt="card-image" class="object-cover w-full h-full" />
-    </div>
-    <div class="p-6">
-      <h6 class="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700 uppercase">
-        startups
-      </h6>
-      <h4 class="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-        Lyft launching cross-platform service this week
-      </h4>
-      <p class="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
-        Like so many organizations these days, Autodesk is a company in
-        transition. It was until recently a traditional boxed software company
-        selling licenses. Yet its own business model disruption is only part of
-        the story
-      </p>
-      <a href="#" class="inline-block"><button
-          class="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/10 active:bg-gray-900/20"
-          type="button">
-          Learn More<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-            stroke-width="2" class="w-4 h-4">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path>
-          </svg></button></a>
-    </div>
-  </div>
+                <div class="container flex flex-row">
+                    <div class="flex justify-center">
+                        <div class="p-1 ">
+                            <div class="bg-red-100" style="padding-top: 20px;">
+                                <img src="storage/rewards/PngItem_2128990.png" style="width: 50%; margin: 0 auto; py-5 bg-white " />
+                            </div>
+                            <h4 class="mb-2 text-center" style="font-size: 2vw;">
+                                Lyft launching cross-platform service this week!
+                            </h4>
+                            <p class="block mb-8 font-sans text-center" style="font-size: 1.2vw;">
+                                Like so many organizations these days, Autodesk is a company in
+                                transition. It was until recently a traditional boxed software company
+                                selling licenses. Yet its own business model disruption is only part of
+                                the story
+                            </p>
+                            <div class="flex justify-center mt-2">
+                              <a href="#">
+                                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                  Button
+                                </button>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-center">
+                        <div class="p-1 ">
+                            <div class="bg-red-100" style="padding-top: 20px;">
+                                <img src="storage/rewards/PngItem_2128990.png" style="width: 50%; margin: 0 auto; py-5 bg-white " />
+                            </div>
+                            <h4 class="mb-2 text-center" style="font-size: 2vw;">
+                                Lyft launching cross-platform service this week!
+                            </h4>
+                            <p class="block mb-8 font-sans text-center" style="font-size: 1.2vw;">
+                                Like so many organizations these days, Autodesk is a company in
+                                transition. It was until recently a traditional boxed software company
+                                selling licenses. Yet its own business model disruption is only part of
+                                the story
+                            </p>
+                            <div class="flex justify-center mt-2">
+                              <a href="#">
+                                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                  Button
+                                </button>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container flex flex-row">
+                    <div class="flex justify-center">
+                        <div class="p-1 ">
+                            <div class="bg-red-100" style="padding-top: 20px;">
+                                <img src="storage/rewards/PngItem_2128990.png" style="width: 50%; margin: 0 auto; py-5 bg-white " />
+                            </div>
+                            <h4 class="mb-2 text-center" style="font-size: 2vw;">
+                                Lyft launching cross-platform service this week!
+                            </h4>
+                            <p class="block mb-8 font-sans text-center" style="font-size: 1.2vw;">
+                                Like so many organizations these days, Autodesk is a company in
+                                transition. It was until recently a traditional boxed software company
+                                selling licenses. Yet its own business model disruption is only part of
+                                the story
+                            </p>
+                            <div class="flex justify-center mt-2">
+                              <a href="#">
+                                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                  Button
+                                </button>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-center">
+                        <div class="p-1 ">
+                            <div class="bg-red-100" style="padding-top: 20px;">
+                                <img src="storage/rewards/PngItem_2128990.png" style="width: 50%; margin: 0 auto; py-5 bg-white " />
+                            </div>
+                            <h4 class="mb-2 text-center" style="font-size: 2vw;">
+                                Lyft launching cross-platform service this week!
+                            </h4>
+                            <p class="block mb-8 font-sans text-center" style="font-size: 1.2vw;">
+                                Like so many organizations these days, Autodesk is a company in
+                                transition. It was until recently a traditional boxed software company
+                                selling licenses. Yet its own business model disruption is only part of
+                                the story
+                            </p>
+                            <div class="flex justify-center mt-2">
+                              <a href="#">
+                                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                  Button
+                                </button>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container flex flex-row">
+                    <div class="flex justify-center">
+                        <div class="p-1 ">
+                            <div class="bg-red-100" style="padding-top: 20px;">
+                                <img src="storage/rewards/PngItem_2128990.png" style="width: 50%; margin: 0 auto; py-5 bg-white " />
+                            </div>
+                            <h4 class="mb-2 text-center" style="font-size: 2vw;">
+                                Lyft launching cross-platform service this week!
+                            </h4>
+                            <p class="block mb-8 font-sans text-center" style="font-size: 1.2vw;">
+                                Like so many organizations these days, Autodesk is a company in
+                                transition. It was until recently a traditional boxed software company
+                                selling licenses. Yet its own business model disruption is only part of
+                                the story
+                            </p>
+                            <div class="flex justify-center mt-2">
+                              <a href="#">
+                                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                  Button
+                                </button>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex justify-center">
+                        <div class="p-1 ">
+                            <div class="bg-red-100" style="padding-top: 20px;">
+                                <img src="storage/rewards/PngItem_2128990.png" style="width: 50%; margin: 0 auto; py-5 bg-white " />
+                            </div>
+                            <h4 class="mb-2 text-center" style="font-size: 2vw;">
+                                Lyft launching cross-platform service this week!
+                            </h4>
+                            <p class="block mb-8 font-sans text-center" style="font-size: 1.2vw;">
+                                Like so many organizations these days, Autodesk is a company in
+                                transition. It was until recently a traditional boxed software company
+                                selling licenses. Yet its own business model disruption is only part of
+                                the story
+                            </p>
+                            <div class="flex justify-center mt-2">
+                              <a href="#">
+                                <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                  Button
+                                </button>
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-  <div class="relative flex bg-clip-border rounded-xl bg-white text-gray-700 shadow-md w-full max-w-[48rem] flex-row">
-    <div
-      class="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none bg-clip-border rounded-xl shrink-0">
-      <img
-        src="https://tailwindcss.com/img/card-top.jpg"
-        alt="card-image" class="object-cover w-full h-full" />
-    </div>
-    <div class="p-6">
-      <h6 class="block mb-4 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-gray-700 uppercase">
-        startups
-      </h6>
-      <h4 class="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-        Lyft launching cross-platform service this week
-      </h4>
-      <p class="block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
-        Like so many organizations these days, Autodesk is a company in
-        transition. It was until recently a traditional boxed software company
-        selling licenses. Yet its own business model disruption is only part of
-        the story
-      </p>
-      <a href="#" class="inline-block"><button
-          class="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none hover:bg-gray-900/10 active:bg-gray-900/20"
-          type="button">
-          Learn More<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-            stroke-width="2" class="w-4 h-4">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path>
-          </svg></button></a>
-    </div>
-  </div>
-
-</div>
 
 </x-app-layout>
